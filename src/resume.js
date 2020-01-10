9af8f53837e4f161a3d2a81fced56a37f0eed53e
David LJ <mail@davidlj95.com>
2020-01-10 02:26:34 +0100

    feat: make navigation drawer responsive
    drawer dismissible if >900px, modal otherwise
    make all top app bar icons disappear when no js
    fix just print / screen CSS selectors
    allow body scroll in print view to print everything
    
    Based on https://glitch.com/~material-responsive-drawer
    
    Closes #9

--- src/resume.js
+++ src/resume.js
@@ -2,18 +2,18 @@
 document.querySelectorAll('.mdc-expansion-panel').forEach((expansionPanel) => {
     const summary = expansionPanel.querySelector('.mdc-expansion-panel__summary');
     const details = expansionPanel.querySelector('.mdc-expansion-panel__details');
-    if(!summary || !details) {
+    if (!summary || !details) {
         console.warn('An expansion panel does not have summary or details');
         console.log(expansionPanel);
         return;
     }
     // Initialize expansion attribute
-    if(!expansionPanel.hasAttribute("aria-expanded")) {
+    if (!expansionPanel.hasAttribute("aria-expanded")) {
         expansionPanel.setAttribute("aria-expanded", "true");
     }
     // Toggle on click
     summary.addEventListener('click', () => {
-        if(expansionPanel.getAttribute("aria-expanded") === "true") {
+        if (expansionPanel.getAttribute("aria-expanded") === "true") {
             expansionPanel.setAttribute("aria-expanded", "false");
         } else {
             expansionPanel.setAttribute("aria-expanded", "true");
@@ -21,15 +21,122 @@ document.querySelectorAll('.mdc-expansion-panel').forEach((expansionPanel) => {
     })
 });
 
-// Material Drawer
+// Material Drawer and Top App Bar
+// https://glitch.com/~material-responsive-drawer
 import {MDCDrawer} from "@material/drawer";
-const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
-
-// Material Top app bar
 import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCList} from "@material/list";
+
+// Select DOM elements
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
-topAppBar.setScrollTarget(document.getElementById('resume'));
-topAppBar.listen('MDCTopAppBar:nav', () => {
-    drawer.open = !drawer.open;
-});
+const listEl = document.querySelector('.mdc-drawer .mdc-list');
+const drawerElement = document.querySelector('.mdc-drawer');
+const mainContentEl = document.querySelector('.main-content');
+
+// Create elements
+let drawer;
+let list;
+let topAppBar;
+
+const createElements = () => {
+    drawer = MDCDrawer.attachTo(drawerElement);
+    list = new MDCList(listEl);
+    list.wrapFocus = true;
+    topAppBar = MDCTopAppBar.attachTo(topAppBarElement);
+    topAppBar.setScrollTarget(mainContentEl);
+    topAppBar.listen('MDCTopAppBar:nav', () => {
+        drawer.open = !drawer.open;
+    });
+};
+
+const closeDrawer = () => {
+    drawer.open = false;
+};
+
+const destroyElements = () => {
+    if(drawer) drawer.destroy();
+    if(list) list.destroy();
+    if(topAppBar) topAppBar.destroy();
+};
+
+const getScrims = () => {
+    const scrims = document.getElementsByClassName('mdc-drawer-scrim')
+    return Array.from(scrims);
+};
+
+const createScrim = () => {
+    const scrims = getScrims();
+    if(!scrims.length) {
+        drawerElement.insertAdjacentHTML("afterend", "<div class=\"mdc-drawer-scrim js-only\"></div>");
+    }
+};
+
+const removeScrims = () => {
+    const scrims = getScrims();
+    scrims.forEach(scrim => scrim.remove());
+};
+
+
+// Initialize either modal or permanent drawer
+const initModalDrawer = () => {
+
+    // Destroy elements
+    destroyElements();
+
+    // Switch drawer
+    drawerElement.classList.remove("mdc-drawer--dismissible", "mdc-drawer--open");
+    drawerElement.classList.add("mdc-drawer--modal");
+    createScrim();
+
+    // Create elements
+    createElements();
+
+    // Hide
+    drawer.open = false;
+
+    // Hide on list click
+    listEl.removeEventListener('click', closeDrawer);
+    listEl.addEventListener('click', closeDrawer);
+};
+
+const initPermanentDrawer = () => {
+
+    // Destroy elements
+    destroyElements();
+
+    // Switch drawer type
+    removeScrims();
+    drawerElement.classList.remove("mdc-drawer--modal");
+    drawerElement.classList.add("mdc-drawer--dismissible");
+
+    // Create elements
+    createElements();
+
+    // Open drawer
+    drawer.open = true;
+
+    // Hide on list click
+    listEl.removeEventListener('click', closeDrawer);
+};
+
+window.matchMedia("(max-width: 900px)").matches ?
+    initModalDrawer() : initPermanentDrawer();
+
+// Toggle between permanent drawer and modal drawer at breakpoint 900px
+const resizeHandler = () => {
+    if (window.matchMedia("(max-width: 900px)").matches) {
+        initModalDrawer();
+    } else {
+        initPermanentDrawer();
+    }
+};
+window.addEventListener('resize', resizeHandler);
+
+// Legacy
+// const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+// const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+// const topAppBar = new MDCTopAppBar(topAppBarElement);
+// topAppBar.setScrollTarget(document.getElementById('resume'));
+// topAppBar.listen('MDCTopAppBar:nav', () => {
+//     drawer.open = !drawer.open;
+// });
